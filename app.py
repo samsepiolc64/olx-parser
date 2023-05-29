6c1a899f3a75ef30971f405f25402f771e85df9f
samsepiolc64 <mjarmuzek@wp.pl>
2023-05-29 18:13:03 +0200

    init

--- app.py
+++ app.py
@@ -257,7 +257,7 @@ def setup():
     base.create_db(getenv('SQL_DROP_OFFER'))
     # rem if initiate OFFER table
     # base.create_db(getenv('SQL_DEL_OFFER'))
-    # base.create_db(getenv('SQL_OFFER'))
+    base.create_db(getenv('SQL_OFFER'))
     # base.create_db(getenv('SQL_XLSX'))
     return render_template('index.html', info="drop old and create new tables")
 
